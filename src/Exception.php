b7207439e19a83e451400135c73819e170574470
Alexey Borzov <avb@php.net>
2024-03-03 13:31:05 +0300

    Add exception classes

---
+++ src/Exception.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of sad-spirit/quickform2-json-renderer package
+ *
+ * (c) Alexey Borzov <avb@php.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace sad_spirit\html_quickform2\json_renderer;
+
+/**
+ * Interface that should be implemented by all package exceptions
+ */
+interface Exception extends \Throwable
+{
+}
---
+++ src/exceptions/InvalidArgumentException.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of sad-spirit/quickform2-json-renderer package
+ *
+ * (c) Alexey Borzov <avb@php.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace sad_spirit\html_quickform2\json_renderer\exceptions;
+
+use sad_spirit\html_quickform2\json_renderer\Exception;
+
+/**
+ * Namespaced version of SPL's InvalidArgumentException
+ */
+class InvalidArgumentException extends \InvalidArgumentException implements Exception
+{
+}
